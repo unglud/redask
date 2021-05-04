6f9cc7dda6e3c75038a18c4086bbf0ce040a11ec
Aleksandr Matrosov <unglud@gmail.com>
2021-05-04 11:40:07 +0200

    connect redux

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,22 @@
-import { Component } from 'react';
+import { Component } from "react";
+import { fetchTodos, Todo } from "../actions";
+import { StoreState } from "../reducers";
+import { connect } from "react-redux";
 
-export class App extends Component {
+interface AppProps {
+  todos: Todo[];
+
+  fetchTodos(): any;
+}
+
+class _App extends Component<AppProps> {
   render() {
     return <div>Hi There</div>;
   }
 }
+
+const mapStateToProps = (state: StoreState): { todos: Todo[] } => {
+  return { todos: state.todos };
+};
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
