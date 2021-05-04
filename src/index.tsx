19fa198e86d16e0b2f7fb7a8c68c35681f9bb006
Aleksandr Matrosov <unglud@gmail.com>
2021-05-04 09:34:04 +0200

    methods

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,30 @@
-import { Component } from 'react';
-import ReactDom from 'react-dom';
+import { Component } from "react";
+import ReactDom from "react-dom";
 
 interface AppProps {
   color?: string;
 }
 
 class App extends Component<AppProps> {
-  state = { counter: 0 }
+  state = { counter: 0 };
 
-  render () {
+  render() {
     return (
       <div>
         <button onClick={this.onIncrement}>++</button>
         <button onClick={this.onDecrement}>--</button>
         {this.state.counter}
       </div>
-    )
+    );
   }
 
   private onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 })
+    this.setState({ counter: this.state.counter + 1 });
   };
 
   private onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 })
+    this.setState({ counter: this.state.counter - 1 });
   };
 }
 
-ReactDom.render(<App/>, document.getElementById('root'));
+ReactDom.render(<App />, document.getElementById("root"));
