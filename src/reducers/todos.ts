5d64890afa9bd2ba6e0ba65ae915f96fbf98cf8a
Aleksandr Matrosov <unglud@gmail.com>
2021-05-04 16:06:53 +0200

    add delete to reducer

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -4,6 +4,8 @@ export const todosReducer = (state: Todo[] = [], action: Action) => {
   switch (action.type) {
     case ActionType.fetchTodos:
       return action.payload;
+    case ActionType.deleteTodo:
+      return state.filter((todo: Todo)=> todo.id !== action.payload);
     default:
       return state;
   }
